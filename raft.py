c92e281aa7e0ac7bed9976463109e1d4708705a5
Abhijit Mondal <amondal@microsoft.com>
2022-12-13 13:28:47 +0530

    changes

--- raft.py
+++ raft.py
@@ -71,7 +71,7 @@ class Raft:
         self.next_indices_lock = Lock()
         self.leader_id_lock = Lock()
         self.commit_index_lock = Lock()
-        self.election_lock = Lock()
+        self.raft_lock = Lock()
         
         utils.run_thread(fn=self.wait_for_servers, args=())
         
@@ -106,6 +106,12 @@ class Raft:
     
     def on_election_timeout(self):
         while True:
+            # Check everytime that state is either FOLLOWER or CANDIDATE before sending
+            # vote requests.
+            
+            # The possibilities in this path are:
+            # 1. Requestor sends requests, receives replies and becomes leader
+            # 2. Requestor sends requests, receives replies and becomes follower again, repeat on election timeout
             if time.time() > self.election_timeout and \
                 (self.state == 'FOLLOWER' or self.state == 'CANDIDATE'):
                 
@@ -116,31 +122,36 @@ class Raft:
     def start_election(self):
         print("Starting election...")
         
+        # At the start of election, set state to CANDIDATE and increment term
+        # also vote for self.
         self.state = 'CANDIDATE'
         self.current_term += 1
         self.voted_for = self.server_index
         self.votes.add(self.server_index)
         
+        # Send vote requests in parallel
         threads = []
         for j in range(len(self.partitions[self.cluster_index])):
             if j != self.server_index:
                t =  utils.run_thread(fn=self.request_vote, args=(j,))
                threads += [t]
         
+        # Wait for completion of request flow
         for t in threads:
             t.join()
         
         return True
     
     def request_vote(self, server):
+        # Get last index and term from commit log
         last_index, last_term = self.commit_log.get_last_index_term()
         
         while True:
+            # Retry on timeout
             print(f"Requesting vote from {server}...")
             
-            if time.time() <= self.election_timeout and \
-                (self.state == 'FOLLOWER' or self.state == 'CANDIDATE'):
-                    
+            # Check if state if still CANDIDATE
+            if self.state == 'CANDIDATE':
                 resp = \
                     utils.send_and_recv_no_retry(f"VOTE-REQ {self.server_index} {self.current_term} {last_term} {last_index}", 
                                                  self.conns[self.cluster_index], 
@@ -148,6 +159,7 @@ class Raft:
                                                  server, 
                                                  timeout=self.rpc_period_ms/1000.0)
                 
+                # If timeout happens resp returns None, so it won't go inside this condition
                 if resp:
                     vote_rep = re.match('^VOTE-REP ([0-9]+) ([0-9\-]+) ([0-9\-]+)$', resp)
                     
@@ -167,48 +179,73 @@ class Raft:
         print(f"Processing vote request from {server} {term}...")
         
         if term > self.current_term:
+            # Requestor term is higher hence update
             self.step_down(term)
-        else:
-            self_last_index, self_last_term = self.commit_log.get_last_index_term()
+
+        # Get last index and term from log
+        self_last_index, self_last_term = self.commit_log.get_last_index_term()
+        
+        # Vote for requestor only if requestor term is equal to self term
+        # and self has either voted for no one yet or has voted for same requestor (can happen during failure/timeout and retry)
+        # and either both requestor and self have empty logs (starting up)
+        # or the last term of requestor is greater 
+        # or if they are equal then the last index of requestor should be greater.
+        # This is to ensure that only vote for all requestors who have updated logs.
+        if term == self.current_term \
+            and (self.voted_for == server or self.voted_for == -1) \
+            and ((last_index == -1 and self_last_index == -1) \
+                or last_term > self_last_term \
+                or (last_term == self_last_term and last_index > self_last_index)):
             
-            if term == self.current_term \
-                and (self.voted_for == server or self.voted_for == -1) \
-                and ((last_index == -1 and self_last_index == -1) \
-                    or last_term > self_last_term \
-                    or (last_term == self_last_term and last_index > self_last_index)):
-                
-                self.voted_for = server
+            self.voted_for = server
         
         return f"VOTE-REP {self.server_index} {self.current_term} {self.voted_for}"
         
     def process_vote_reply(self, server, term, voted_for):
         print(f"Processing vote reply from {server} {term}...")
         
+        # It is not possible to have term < self.current_term because during vote request
+        # the server will update its term to match requestor term if requestor term is higher
         if term > self.current_term:
+            # Requestor term is lower hence update
             self.step_down(term)
         
-        elif term == self.current_term and self.state == 'CANDIDATE':
+        if term == self.current_term and self.state == 'CANDIDATE':
             if voted_for == self.server_index:
                 self.votes.add(server)
             
+            # Convert to leader if received votes from majority
             if len(self.votes) > len(self.partitions[self.cluster_index])/2.0:
                 self.state = 'LEADER'
                 self.leader_id = self.server_index
+                
+                # Update state machine i.e. in memory hash map
+                for term, command in self.commit_log.read_log():
+                    self.update_state_machine(command)
+                
                 print(f"{self.cluster_index}-{self.server_index} became leader")
                 print(f"{self.votes}-{self.current_term}")
                 
     def step_down(self, term):
         print(f"Stepping down...")
+        
+        # Revert to follower state
         self.current_term = term
         self.state = 'FOLLOWER'
         self.voted_for = -1
+        
+        # Once server becomes follower, set election timeout again
+        self.set_election_timeout()
     
     def leader_send_append_entries(self):
         print(f"Sending append entries from leader...")
         
         while True:
+            # Check everytime if it is leader before sending append queries
             if self.state == 'LEADER':
                 self.append_entries()
+                
+                # Commit entry after it has been replicated
                 last_index, _ = self.commit_log.get_last_index_term()
                 self.commit_index = last_index
                     
@@ -217,6 +254,7 @@ class Raft:
             
         for j in range(len(self.partitions[self.cluster_index])):
             if j != self.server_index:
+                # Send append entries requests in parallel
                 utils.run_thread(fn=self.send_append_entries_request, args=(j,res,))
                 
         cnts = 0
@@ -225,6 +263,8 @@ class Raft:
             # Wait for servers to respond
             res.get(block=True)
             cnts += 1
+            # Once we get reply from majority of servers, then return
+            # and don't wait for remaining servers
             # Exclude self
             if cnts > (len(self.partitions[self.cluster_index])/2.0)-1:
                 return
@@ -233,7 +273,11 @@ class Raft:
     def send_append_entries_request(self, server, res=None):
         print(f"Sending append entries to {server}...")
         
+        # Fetch previous index and previous term for log matching
         prev_idx = self.next_indices[server]-1
+        
+        # Get all logs from prev_idx onwards, because all logs after prev_idx will be
+        # used to replicate to server
         log_slice = self.commit_log.read_logs_start_end(prev_idx)
         
         if prev_idx == -1:
@@ -253,6 +297,7 @@ class Raft:
                                                 server, 
                                                 timeout=self.rpc_period_ms/1000.0)  
                 
+                # If timeout happens resp returns None, so it won't go inside this condition
                 if resp:
                     append_rep = re.match('^APPEND-REP ([0-9]+) ([0-9\-]+) ([0-9]+) ([0-9\-]+)$', resp)
                     
@@ -277,19 +322,39 @@ class Raft:
         
         flag, index = 0, 0
         
+        # If term < self.current_term then the append request came from an old leader
+        # and we should not take action in that case.
+        
         if term > self.current_term:
+            # Most likely term == self.current_term, if this server participated 
+            # in voting rounds. If server was down during voting rounds and previous append requests, then term > self.current_term
+            # and we should update its term 
             self.step_down()
         
         if term == self.current_term:
+            # Request came from current leader
             self.leader_id = server
             
+            # Check if the term corresponding to the prev_idx matches with that of the leader
             self_logs = self.commit_log.read_logs_start_end(prev_idx, prev_idx) if prev_idx != -1 else []
+            
+            # Even with retries, this is idempotent
             success = prev_idx == -1 or (len(self_logs) > 0 and self_logs[0][0] == prev_term)
             
             if success:
-                index = self.store_entries(prev_idx, logs)
+                # On retry, we will overwrite the same logs
+                last_index, last_term = self.commit_log.get_last_index_term()
+                
+                if len(logs) > 0 and last_term == logs[-1][0] and last_index == self.commit_index:
+                    # Check if last term in self log matches leader log and last index in self log matches commit index
+                    # Then this is a retry and will avoid overwriting the logs
+                    index = self.commit_index
+                else:
+                    index = self.store_entries(prev_idx, logs)
                 
             flag = 1 if success else 0
+            
+            # Update election timeout because we received heartbeat from leader
             self.set_election_timeout()
         
         return f"APPEND-REP {self.server_index} {self.current_term} {flag} {index}"
@@ -297,27 +362,41 @@ class Raft:
     def process_append_reply(self, server, term, success, index):
         print(f"Processing append reply from {server} {term}...")
         
+        # It cannot be possible that term < self.current_term because at the time of append request, 
+        # all servers will be updated to current term
+        
         if term > self.current_term:
+            # This could be an old leader which become active and sent
+            # append requests but on receiving higher term, it will revert
+            # back to follower state.
             self.step_down(term)
             
-        elif self.state == 'LEADER' and term == self.current_term:
+        if self.state == 'LEADER' and term == self.current_term:
             if success:
+                # If server log repaired successfully from leader then increment next index
                 self.next_indices[server] = index+1
             else:
+                # If server log could not be repaired successfully, then retry with 1 index less lower than current next index
+                # Process repeats until we find a matching index and term on server
                 self.next_indices[server] = max(0, self.next_indices[server]-1)
                 self.send_append_entries_request(server)  
                 
     def store_entries(self, prev_idx, leader_logs):
+        # Update/Repair server logs from leader logs, replacing non-matching entries and adding non-existent entries
+        # Repair starts from prev_idx+1 where prev_idx is the index till where
+        # both leader and server logs match.
         commands = [f"{leader_logs[i][1]}" for i in range(len(leader_logs))]
         last_index, _ = self.commit_log.log_replace(self.current_term, commands, prev_idx+1)
         self.commit_index = last_index
         
+        # Update state machine
         for command in commands:
             self.update_state_machine(command)
         
         return last_index
     
     def update_state_machine(self, command):
+        # Update state machine i.e. in memory hash map in this case
         set_ht = re.match('^SET ([^\s]+) ([^\s]+) ([0-9]+)$', command)
             
         if set_ht:
